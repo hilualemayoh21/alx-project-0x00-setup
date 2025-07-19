a60dc1a070818c6504169c3575be5fab2211eb17
hlina alemayoh <hilubecoder@gmail.com>
2025-07-19 21:13:40 +0300

    fix some issues

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -11,7 +11,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-  const   Home: React.FC = ()=>{
+  const Home: React.FC = () => {
     return(
     <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
         <h1>Airbnb Application Clone system</h1>
